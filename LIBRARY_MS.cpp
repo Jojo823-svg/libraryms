d1254d4bd1ef5329807e26c92a3f92e6b0563692
Jojo823-svg <joy.mutinda@strathmore.edu>
2025-02-19 06:18:29 +0300

    Abstract and base class parent

---
+++ LIBRARY_MS.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <stdexcept>
+#include <fstream>
+
+using namespace std;
+
+class LibraryEntity {//abstract class
+public://access specifier, polymorphism concept 
+    virtual void displayDetails() const = 0;//pure virtual functionsS
+    virtual void saveToFile(ofstream &outFile) const = 0;
+    virtual void loadFromFile(ifstream &inFile) = 0;
+};
+
+class Person {//base class
+protected://access specifier, encapsulation concept
+    string name;
+    int id;
+    string contact;
+    string role;
+public://access specifier
+    Person(string n, int i, string c, string r) : name(n), id(i), contact(c), role(r) {}//constructor initializing the attributes
+    int getId() const { return id; }
+    string getRole() const { return role; }
+    virtual void displayDetails() const {//virtual function
+        cout << "Name: " << name << ", ID: " << id << ", Contact: " << contact << ", Role: " << role << endl;
+    }
+};
